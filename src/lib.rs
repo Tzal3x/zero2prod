fe046f704673a7e8b3c2d8b6d56581ecaa859759
Alexandros Tzimas <tzalex@outlook.com.gr>
2024-01-07 18:31:57 +0200

    Fix connection refused error using random port assignment

---
+++ src/lib.rs
@@ -0,0 +1,28 @@
+use actix_web::{web, App, HttpResponse, HttpServer};
+use actix_web::dev::Server;
+use std::net::TcpListener;
+
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String
+}
+
+async fn subscribe() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
+            App::new()
+                .route("/health_check", web::get().to(health_check))
+                .route("/subscriptions", web::post().to(subscribe))
+        })
+        .listen(listener)?
+        .run();
+        Ok(server)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use std::net::TcpListener;
+
 use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1")
+        .expect("Failed to bind random port");
+    run(listener)?.await
 }
